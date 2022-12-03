5651cee65aa7f4e989fe5bc75cfd5b733ae27279
ualan <ualanarend49@gmail.com>
2022-12-03 18:01:48 -0300

    att quartas 2

--- quartas.js
+++ quartas.js
@@ -26,7 +26,7 @@ document.querySelector("#cards").innerHTML =
     "09/12",
     "sexta",
     createGame("undefined", "12:00", "undefined") +
-      createGame("holanda", "16:00", "undefined")
+      createGame("holanda", "16:00", "argentina")
   ) +
   createCard(
     "10/12",
